7b7f811edbca30a334e526beecd3cd41631c3bfb
Cas0n-McVey <absolgamer25@gmail.com>
2025-04-10 14:03:02 -0500

    Static

---
+++ Calculate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_McVey_Cason
+{
+    static class Calculate
+    {
+        // equal to 0.0f
+        public static float result = 0.0f;
+
+        // Static constructor is equal to 0.0f too
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+
+        // INTEGERS
+        
+        // Static integers of Add, Sub, Mult, and Div and return it
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+
+        // FLOATING
+
+        // Static float of Add, Sub, Mult, and Div and return it
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Static_McVey_Cason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Integers
+            // Prints integers Add, Sub, Mult, and Div
+            Console.WriteLine("Integers Methods");
+            Console.WriteLine("21 + 65 is " + Calculate.Add(21, 65));
+            Console.WriteLine("50 - 32 is " + Calculate.Sub(50, 32));
+            Console.WriteLine("20 * 8 is " + Calculate.Mult(20, 8));
+            Console.WriteLine("95 / 12 is " + Calculate.Div(92, 12));
+
+            // Floats
+            // Prints Float Add, Sub, Mult, and Div
+            Console.WriteLine("\nFloat Methods");
+            Console.WriteLine("28.22 + 75.75 is " + Calculate.Add(28.22f, 75.75f));
+            Console.WriteLine("25.25 - 32.32 is " + Calculate.Sub(25.25f, 32.32f));
+            Console.WriteLine("25.20 * 5.0 is " + Calculate.Mult(25.20f, 5.0f));
+            Console.WriteLine("100.925 / 12.21 is " + Calculate.Div(100.925f, 12.21f));
+        }
+    }
+}
